a6503086e12c9d9b81f1cf9fc6666417bda2d12a
Jacob-Tuttle <147278166+Jacob-Tuttle@users.noreply.github.com>
2023-11-27 12:34:05 -0800

    Update home-page.py

--- website/home-page.py
+++ website/home-page.py
@@ -25,8 +25,41 @@ if uploaded_file is not None:
 
 
     # Can be used wherever a "file-like" object is accepted:https://github.com/Jacob-Tuttle/ECS171-Project/blob/Website/website/home-page.py
-    dataframe = pd.read_csv(uploaded_file)
-    DF = pd.DataFrame(dataframe)
-    DF
-    pairplot = sns.pairplot(DF)
-    st.pyplot(pairplot)
+    data = pd.read_csv(uploaded_file)
+    
+    drop = ['ID', 'Customer_ID', 'Month', 'Name', 'SSN', 'Occupation', 'Type_of_Loan', 'Credit_Mix', 'Credit_History_Age', 'Payment_of_Min_Amount', 'Payment_Behaviour',
+        'Age', 'Annual_Income', 'Num_Bank_Accounts', 'Num_Credit_Card', 'Interest_Rate', 'Num_of_Loan',  'Num_Credit_Inquiries',
+        'Total_EMI_per_month', 'Amount_invested_monthly', 'Num_of_Delayed_Payment']
+
+    # Columns to clean
+    clean = ['Delay_from_due_date', 'Monthly_Inhand_Salary', 'Monthly_Balance', 'Changed_Credit_Limit','Outstanding_Debt', 'Credit_Utilization_Ratio', 'Credit_History_Age']
+    
+    # Clean columns
+    for item in clean:
+        data[item] = pd.to_numeric(data[item], errors='coerce')
+    
+    # Map credit scores to number
+    creditScoreMap = {'Poor': 1, 'Standard': 2, 'Good': 3}
+    data['Credit_Score'] = data['Credit_Score'].replace(creditScoreMap)
+    
+    
+    cleanedData = train.copy().drop(columns=drop)
+    # Drop entries with NaN values
+    cleanedData.dropna(inplace=True)
+    # Separate features (X) and target variable (y)
+    X = cleanedData.drop(columns=['Credit_Score'])
+    y = cleanedData['Credit_Score']
+    
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    print("X_train")
+    print(X_train)
+    print("y_test")
+    print(y_train)
+    print("X_test")
+    print(X_test)
+    print("y_test")
+    print(y_test)
+    
+
