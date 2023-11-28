956542293274fae5baecb48f7fa8e799b1deef58
Jacob-Tuttle <147278166+Jacob-Tuttle@users.noreply.github.com>
2023-11-27 21:27:07 -0800

    Update home-page.py

--- website/home-page.py
+++ website/home-page.py
@@ -10,8 +10,29 @@ from sklearn.metrics import classification_report, accuracy_score, confusion_mat
 from sklearn.linear_model import LogisticRegression
 
 from outliers import removeOutliers
-import logistic
 import svm
+
+def report(data):
+    X = data.drop(columns=['Credit_Score'])
+    y = data['Credit_Score']
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Create a multiclass logistic regression model
+    logreg = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+
+    # Fit the model on the training data
+    logreg.fit(X_train, y_train)
+
+    # Make predictions on the test data
+    predictions = logreg.predict(X_test)
+
+    # Evaluate the model
+    # print("Accuracy:", accuracy_score(y_test, predictions))
+    # print("Confusion Matrix:\n", confusion_matrix(y_test, predictions))
+    # print("Classification Report:\n", classification_report(y_test, predictions))
+    return classification_report(y_test, predictions)
 
 st.header('ECS-171 Project - Classify Credit Score', divider='blue')
 
