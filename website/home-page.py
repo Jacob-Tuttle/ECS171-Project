cd156bb98dbcc7f348b4d3aa7960432f2a2772fe
Kyle Jow <77653395+kylejow@users.noreply.github.com>
2023-12-07 22:48:12 -0800

    combine models to one nb, upload models, update website

--- website/home-page.py
+++ website/home-page.py
@@ -16,9 +16,9 @@ import svm
 
 st.header('ECS-171 Project - Classify Credit Score', divider='blue')
 
-logistic = joblib.load("website/logistic_model.pkl")
-svmLinear = joblib.load("website/svm_linear_model.pkl")
-svmRBF = joblib.load("website/svm_rbf_model.pkl")
+logistic = joblib.load("models/logistic_no_outliers")
+svmLinear = joblib.load("models/svc_linear_no_outliers")
+svmRBF = joblib.load("models/svc_rbf_no_outliers")
 
 delay_from_due_date = st.slider("Delay from Due Date", 0, 50, 1)
 num_of_delayed_payment = st.slider("Number of Delayed Payments", 0, 20, 1)
@@ -39,10 +39,6 @@ logicPred = logistic.predict(df)
 svmLinearPred = svmLinear.predict(df)
 svmRBFPred = svmRBF.predict(df)
 
-dict = {}
-dict[1] = "Bad"
-dict[2] = "Standard"
-dict[3] = "Good"
-st.write(f"Logistic Predicted Credit Score: {dict[logicPred[0]]}")
-st.write(f"Linear SVM Predicted Credit Score: {dict[svmLinearPred[0]]}")
-st.write(f"Non-Linear SVM Predicted Credit Score: {dict[svmRBFPred[0]]}")
+st.write(f"Logistic Predicted Credit Score: {logicPred[0]}")
+st.write(f"Linear SVM Predicted Credit Score: {svmLinearPred[0]}")
+st.write(f"Non-Linear SVM Predicted Credit Score: {svmRBFPred[0]}")
